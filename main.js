24909eb391c49494d7b44eedd7f27a0538023f04
Alan Turing <anderson.gregory.mail@gmail.com>
2019-05-15 23:05:37 -0600

    Add button disable, listeners for keyup event, appending div

--- main.js
+++ main.js
@@ -3,32 +3,32 @@ var inputRangeMin = document.querySelector('#js_range__input--min');
 var inputRangeMax = document.querySelector('#js_range__input--max');
 var outputRangeMin = document.querySelector('#player__span--min');
 var outputRangeMax =  document.querySelector('#player__span--max');
-
 var inputNameCh1 = document.querySelector('#player__name--ch1');
 var inputNameCh2 = document.querySelector('#player__name--ch2');
 var outputNameCh1 =document.querySelector('#latest-score__span--ch1');
 var outputNameCh2 = document.querySelector('#latest-score__span--ch2');
 var btnSubmit = document.querySelector('#player__btn--submit');
-var playerForm = document.querySelector('#player__form');
+var playerFormCh1 = document.querySelector('#player__form');
+var playerFormCh2 = document.querySelector('.player__form--ch2')
 var btnClear = document.querySelector('#player__btn--clear');
 var btnReset = document.querySelector('#player__btn--reset');
-
 var inputGuessCh1 = document.querySelector('#player__guess--ch1');
 var inputGuessCh2 = document.querySelector('#player__guess--ch2');
 var currentGuessCh1 = document.querySelector('#latest-score__guess--ch1');
 var currentGuessCh2 = document.querySelector('#latest-score__guess--ch2');
-
-// var rangeError = document.querySelector(".range__div")
-
-var randomNum 
-
 var guessHintCh1 = document.querySelector('#latest-score__output--left');
 var guessHintCh2 = document.querySelector('#latest-score__output--right');
 var latestScoreErrorCh1 = document.querySelector("#player__guess--ch1");
 var latestScoreErrorCh2 = document.querySelector("#player__guess--ch2");
-var playerError = document.querySelector("#range__input--error")
+var playerError = document.querySelector("#range__input--error");
+var guessErrorCh1 = document.querySelector ('#ch1__guess--error');
+var guessErrorCh2 = document.querySelector ('#ch2__guess--error');
+var nameErrorCh1 = document.querySelector ('#ch1__name--error');
+var nameErrorCh2 = document.querySelector ('#ch2__name--error');
+var winnerCards = document.getElementById('card__section')
 var minRange;
 var maxRange;
+var randomNum; 
 
 btnRangeUpdate.addEventListener('click', updateCorrectRange);
 btnClear.addEventListener('click', clearPlayerForm);
@@ -40,10 +40,9 @@ btnSubmit.addEventListener('click', function(){
     displayGuess ();
     checkResultsCh1 ();
     checkResultsCh2 ();
+    cardGenerate();
   } 
 });
-
-// make room in a div and hide and toggle
 
 function updateRange() {
   if(inputRangeMin.value >= inputRangeMax.value) {
@@ -82,18 +81,21 @@ function updateCorrectRange() {
 }
 
 function clearPlayerForm() {
-  playerForm.reset();
+  playerFormCh1.reset();
+  playerFormCh2.reset();
+  disableClear.disabled = true;
+  disableReset.disabled = true;
 }
 
 function resetGame() {
+  outputRangeMax.innerHTML = 100;
+  outputRangeMin.innerHTML = 1;
   clearPlayerForm();
   generateRandomNumber();
   changeName();
+  disableClear.disabled = true;
+  disableReset.disabled = true;
 }
-
-
-var guessErrorCh1 = document.querySelector ('#ch1__guess--error');
-var guessErrorCh2 = document.querySelector ('#ch2__guess--error');
 
 function displayGuess (){
 
@@ -127,69 +129,110 @@ function checkUpperLimit(guess){
 function checkResultsCh1 () {
   var playerOneGuess = parseInt(inputGuessCh1.value);
  if (playerOneGuess > randomNum){
-      guessHintCh1.innerHTML = "That's too high!";
-      } else if 
-      (playerOneGuess < randomNum){
-      guessHintCh1.innerHTML = "That's too low!";
-      } else {
-      guessHintCh1.innerHTML = "BOOM!"
-      }
+    guessHintCh1.innerHTML = "That's too high!";
+    } else if 
+    (playerOneGuess < randomNum){
+    guessHintCh1.innerHTML = "That's too low!";
+    } else {
+    guessHintCh1.innerHTML = "BOOM!"
+    }
 }
 
-function checkResultsCh2 () {
+function checkResultsCh2() {
   var playerTwoGuess = parseInt(inputGuessCh2.value);
- if (playerTwoGuess > randomNum){
-      guessHintCh2.innerHTML = "That's too high!";
-      } else if 
-    (playerTwoGuess < randomNum){
-      guessHintCh2.innerHTML = "That's too low!";
-      } else {
-      guessHintCh2.innerHTML = "BOOM!";
+  if (playerTwoGuess > randomNum){
+    guessHintCh2.innerHTML = "That's too high!";
+    } else if (playerTwoGuess < randomNum){
+    guessHintCh2.innerHTML = "That's too low!";
+    } else {
+    guessHintCh2.innerHTML = "BOOM!";
 }
 }
 
-var nameErrorCh1 = document.querySelector ('#ch1__name--error');
-var nameErrorCh2 = document.querySelector ('#ch2__name--error');
-
-// function onlyAlphaNumeric (input){
-//   onlyAlphaCh1 ();
-//   onlyAlphaCh2 ();
-// } 
-
-function onlyAlphaCh1 (input){
+function onlyAlphaCh1(input) {
   var lettersNumbers = /^[0-9a-zA-Z]+$/;
   if (input.value.match(lettersNumbers)){
-  inputNameCh1.classList.remove('error-border');
-  nameErrorCh1.classList.toggle('hidden', true);
-  return true;
-} else {
-  inputNameCh1.classList.add('error-border')
-  nameErrorCh1.classList.toggle('hidden', false)
-}
+    inputNameCh1.classList.remove('error-border');
+    nameErrorCh1.classList.toggle('hidden', true);
+    return true;
+    } else {
+    inputNameCh1.classList.add('error-border')
+    nameErrorCh1.classList.toggle('hidden', false)
+    }
 }
 
-function onlyAlphaCh2 (input){
+function onlyAlphaCh2(input) {
   var lettersNumbers = /^[0-9a-zA-Z]+$/;
   if (input.value.match(lettersNumbers)){
-  inputNameCh2.classList.remove('error-border');
-  nameErrorCh2.classList.toggle('hidden', true);
-  return true;
-} else {
-  inputNameCh2.classList.add('error-border')
-  nameErrorCh2.classList.toggle('hidden', false)
-}
+    inputNameCh2.classList.remove('error-border');
+    nameErrorCh2.classList.toggle('hidden', true);
+    return true;
+    } else {
+    inputNameCh2.classList.add('error-border')
+    nameErrorCh2.classList.toggle('hidden', false)
+    }
 }
 
+
+function cardGenerate(){
+if (guessHintCh1.innerText === 'BOOM!' || guessHintCh2.innerText === 'BOOM!') {
+    var childCard =`<section class="card__section">
+    <div class="card__div--ch">
+      <span class="card__span--ch1">${inputNameCh1.value}</span>
+      <p>vs</p>
+      <span class="card__span--ch2">${inputNameCh2.value}</span>
+    </div>
+    <div class="card__div--winner">
+      <span class="card__span--name">${winnerName()}</span>
+      <span class="card__span--winner">WINNER</span>
+    </div>
+    <div class="card__div--bottom">
+      <span class="card__span--guess">XX GUESSES</span>
+      <span class="card__span--minutes">XX MINUTES</span>
+      <button class="card__btn--close" type="button">x</button>
+    </div>
+  </section>`
+    winnerCards.insertAdjacentHTML('afterbegin', childCard)
+  }
+}
+
+function winnerName() {
+  var cardName = ""
+  if (guessHintCh1.innerText === 'BOOM!'){
+    cardName = inputNameCh1.value;
+  } else if (guessHintCh2.innerHTML === 'BOOM!'){
+    cardName = inputNameCh2.value;
+  } 
+  return cardName;
+}
+
+
+
+var disableReset = document.querySelector("#player__btn--reset")
+var disableClear = document.querySelector("#player__btn--clear")
+
+
+inputNameCh2.addEventListener('keyup', enableBtn)
+inputNameCh1.addEventListener('keyup', enableBtn)
+inputGuessCh2.addEventListener('keyup', enableBtn)
+inputGuessCh1.addEventListener('keyup', enableBtn)
+
+function enableBtn(event){
+  if (event.target.value === ""){
+    disableClear.disabled = true;
+    disableReset.disabled = true;
+  } else {
+    disableClear.disabled = false;
+    disableReset.disabled = false;
+  }
+}
+
+
+
+//WHITE MARGINS IN BORDER
+//HIDDEN CARD PROBLEM
+//BUTTON COLORS
 
 
 window.onload = generateRandomNumber();
-
-
-
-
-
-
-
-
-
 
